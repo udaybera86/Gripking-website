2f6d277785a6fdf3ce39e4ab8a277590f7a822f3
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-02-07 16:57:40 +0530

    5 o'clock update

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector('video').playbackRate = .95 ;
+
+$('#gallery').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 1000,
+    pauseOnHover: false,
+    cssEase: 'linear'
+  });
