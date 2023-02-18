5fbeb3f7e038f7626c77dd2ba40804fa7af131fd
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-02-18 13:34:25 +0530

    check Description below
    1. review slider js code and js cdn commented
    2. all the html page that we are going to create are added
    3. have code backup in my PC

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
-"use strict";
+// "use strict";
 
 document.querySelector('video').playbackRate = .95;
 
-$(document).ready(function () {
-  $("#testimonial-slider").owlCarousel({
-    items: 2,
-    itemsDesktop: [1000, 2],
-    itemsDesktopSmall: [980, 1],
-    itemsTablet: [768, 1],
-    pagination: true,
-    // navigation:true,
-    // navigationText:["<",">"],
-    autoPlay: true
-  });
-});
+// $(document).ready(function () {
+//   $("#testimonial-slider").owlCarousel({
+//     items: 2,
+//     itemsDesktop: [1000, 2],
+//     itemsDesktopSmall: [980, 1],
+//     itemsTablet: [768, 1],
+//     pagination: true,
+//     // navigation:true,
+//     // navigationText:["<",">"],
+//     autoPlay: true
+//   });
+// });
 
 
 // <--- Dropdown menu hover --->
@@ -27,9 +27,93 @@ $(".dropdown-container").mouseover(function () {
 });
 
 $(".dropdown-container").mouseleave(function () {
-  $(".dropdown-container").slideUp(10);
+  $(".dropdown-container").slideUp();
 });
 
 $("#product").mouseleave(function () {
-  $(".dropdown-container").slideUp(10);
+  $(".dropdown-container").slideUp();
 });
+
+
+
+// $(function() {
+//   // Dropdown toggle
+//   $('#product').mouseenter(function(){
+//     $('.dropdown-container').animate({
+//       top: '200px'
+//     }, 'slow');
+//   });
+
+//   $('#product').mouseleave(function(){
+//     $('.dropdown-container').animate({
+//       top: '-200px'
+//     }, 'slow');
+//   });
+
+//   // $(document).mouse(function(e) {
+//   //   var target = e.target;
+//   //   if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
+//   //     $('.dropdown').toggle(function() {
+//   //     $('.dropdown').stop().animate({
+//   //       top: '-100px'
+//   //     }, 'slow');
+//   //   });
+//   //   }
+//   // });
+// });
+
+// var dropdownToggle = $('#product');
+// var dropdown = dropdownToggle.next('.dropdown-container');
+// $(function() {
+//   dropdown.slideUp(0);
+//   $(document).on('mouseenter', function(e) {
+//     var target = e.target;
+//     if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
+//       if (dropdown.is(':visible')) {
+//         dropdown.stop(true).slideUp();
+//       }
+//     }
+//   });
+//   dropdownToggle.on('mouseleave', function() {
+//     dropdown.stop(true).slideToggle();
+//   });
+// });
+
+
+// $( "#product" ).to(function() {
+//   $( ".dropdown-container").slideDown( "slow", function() {
+//     // Animation complete.
+//   });
+// });
+
+
+  // $(document).ready(function () {
+
+  //   $(".product").click(function () {
+  //     $(".dropdown-container").slideUp(1500);
+  //   });
+
+  //   $(".product").click(function () {
+  //     $(".dropdown-container").slideDown(1500);
+  //   });
+
+  //   $(".product").click(function () {
+  //     $(".dropdown-container").slideToggle(1500);
+  //   });
+
+
+//   $('#product').hover(function () {
+//     // over
+//     $(".dropdown-container").slideDown(1500);
+
+//   }, function () {
+//     // out
+//   }
+//   );
+// });
+
+// $(function () {
+//   $("#product").on("click", function () {
+//     $(".dropdown-container").animate({ top: "200px" });
+//   });
+// });
