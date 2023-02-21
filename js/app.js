c2797f7f86eeb3e4c4bfbac4533b648430c32c04
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-02-21 14:05:50 +0530

    about page added and home page code  is not properly working

--- js/app.js
+++ js/app.js
@@ -17,25 +17,19 @@ $(document).ready(function () {
 
 
 // <--- Dropdown menu hover --->
-
 $("#product").mouseover(function () {
   $(".dropdown-container").slideDown(2000);
 });
 
-$(".dropdown-container").mouseover(function () {
-  $(".dropdown-container").slideDown();
-});
+// $(".dropdown-container").mouseover(function () {
+//   // $(".dropdown-container").slideDown();
+//   // $(".dropdown-container").
+// });
 
 $(".dropdown-container").mouseleave(function () {
   $(".dropdown-container").slideUp(10);
 });
 
-$("#product").mouseleave(function () {
-  $(".dropdown-container").slideUp(10);
-});
-
-
-const nav = document.querySelector('.navbar');
-fetch('/navbar.html').then(res=>res.text()).then(data=>{
-  nav.innerHTML=data;
-})
+// $("#product").mouseleave(function () {
+//   $(".dropdown-container").slideUp(10);
+// });
