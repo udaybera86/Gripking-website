d027e5ea4e2ce4782279e66b91fb0a11ba274134
udaybera86 <115700400+udaybera86@users.noreply.github.com>
2023-03-01 13:51:48 +0530

    updated site

--- js/app.js
+++ js/app.js
@@ -1,39 +1,39 @@
 "use strict";
 
-// <--- Home Section --->
+// <------------------- Home Section ---------------------->
 
 document.querySelector('video').playbackRate = .95;
 
-// <--- Section Four --->
+// <------------------- Section Four ---------------------->
 
-$(document).ready(function () {
-  $("#testimonial-slider").owlCarousel({
-    items: 2,
-    itemsDesktop: [1000, 2],
-    itemsDesktopSmall: [980, 1],
-    itemsTablet: [768, 1],
-    pagination: true,
-    // navigation:true,
-    // navigationText:["<",">"],
-    autoPlay: true
-  });
-});
+// $(document).ready(function () {
+//   $("#testimonial-slider").owlCarousel({
+//     items: 2,
+//     itemsDesktop: [1000, 2],
+//     itemsDesktopSmall: [980, 1],
+//     itemsTablet: [768, 1],
+//     pagination: true,
+//     // navigation:true,
+//     // navigationText:["<",">"],
+//     autoPlay: true
+//   });
+// });
 
 
-// <--- Dropdown menu hover --->
+// <-------------------- Dropdown menu hover -------------->
 
 $("#product").mouseover(function () {
-  $(".dropdown-container").slideDown(800);
+    $(".dropdown-container").slideDown(800);
 });
-
-// $(".dropdown-container").mouseover(function () {
-//   $(".dropdown-container").slideDown();
-// });
 
 $(".dropdown-container").mouseleave(function () {
-  $(".dropdown-container").slideUp(800);
+    $(".dropdown-container").slideUp(800);
 });
 
-// $("#product").mouseleave(function () {
-//   $(".dropdown-container").slideUp(800);
-// });
+$("#codeofconduct").mouseover(function () {
+    $(".dropdown-container").slideUp(800);
+});
+
+$("#about").mouseover(function () {
+    $(".dropdown-container").slideUp(800);
+});
