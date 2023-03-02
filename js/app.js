5a097385534685e0f5293a6e005f56a5a7b0dcd2
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-03-02 14:54:18 +0530

    about section

--- js/app.js
+++ js/app.js
@@ -37,3 +37,25 @@ $("#codeofconduct").mouseover(function () {
 $("#about").mouseover(function () {
     $(".dropdown-container").slideUp(800);
 });
+
+
+// const counters = document.querySelector(".count");
+// const speed = 200;
+
+// counters.forEach((counter) => {
+//   const updateCount = () => {
+//     const target = parseInt(+counter.getAttribute("data-target"));
+//     const count = parseInt(+counter.innerText);
+//     const increment = Math.trunc(target / speed);
+//     console.log(increment);
+//     if (count < target) {
+//       counter.innerText = count + increment;
+//       setTimeout(updateCount, 1);
+//     } else {
+//       count.innerText = target;
+//     }
+//   };
+//   updateCount();
+// });
+
+$('.trust span').countTo();
