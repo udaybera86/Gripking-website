4588682bdad4f17c9702fb2c85b08c1632eab1c9
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-03-02 15:14:15 +0530

    counter

--- js/app.js
+++ js/app.js
@@ -58,4 +58,33 @@ $("#about").mouseover(function () {
 //   updateCount();
 // });
 
-$('.trust span').countTo();
+// $('.trust span').onscroll();
+
+
+var counted = 0;
+$(window).scroll(function () {
+    var oTop = $('.trust').offset().top - window.innerHeight;
+    if (counted == 0 && $(window).scrollTop() > oTop) {
+        $('.count').each(function () {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                countNum: countTo
+            },
+                {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        $this.text(this.countNum);
+                        //alert('finished');
+                    }
+                });
+        });
+        counted = 1;
+    }
+});
