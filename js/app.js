0dce0dae3ea864b9f19bd033746510d246342684
udaybera86 <115700400+udaybera86@users.noreply.github.com>
2023-07-16 15:26:51 +0530

    sdfvsd

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
 "use strict";
 
+const cursor = document.querySelector(".cb-cursor");
+window.onpointermove = event => {
+    const { clientX, clientY } = event;
+
+    cursor.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`
+    }, { duration: 3000, fill: "forwards" })
+}
+
+const elements = [...document.querySelectorAll("h2 div")]
+
+elements.map(element => {
+    element.onmouseover = () => {
+        cursor.classList.add("-video")
+    }
+    element.onmouseout = () => {
+        cursor.classList.remove("-video")
+    }
+})
+
 // <----------------------------------- Dropdown menu hover Start ----------------------------------------->
 
 $("#product").mouseover(function () {
--- js/cursor.js
+++ js/cursor.js
@@ -1,25 +1,23 @@
-	//Page cursors
+//Page cursors
 
-    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
-        t.style.left = n.clientX + "px", 
-		t.style.top = n.clientY + "px", 
-		e.style.left = n.clientX + "px", 
-		e.style.top = n.clientY + "px"
-    });
-    var t = document.getElementById("cursor"),
-        e = document.getElementById("cursor2");
-    function n(t) {
-        e.classList.add("hover")
-    }
-    function s(t) {
-        e.classList.remove("hover")
-    }
-    s();
-    for (var r = document.querySelectorAll(".hover-link"), a = r.length - 1; a >= 0; a--) {
-        o(r[a])
-    }
-    function o(t) {
-        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
-    }
-
-	
+document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+    t.style.left = n.clientX + "px", 
+	t.style.top = n.clientY + "px", 
+	e.style.left = n.clientX + "px", 
+	e.style.top = n.clientY + "px"
+});
+var t = document.getElementById("cursor"),
+    e = document.getElementById("cursor2");
+function n(t) {
+    e.classList.add("hover")
+}
+function s(t) {
+    e.classList.remove("hover")
+}
+s();
+for (var r = document.querySelectorAll(".hover-link"), a = r.length - 1; a >= 0; a--) {
+    o(r[a])
+}
+function o(t) {
+    t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+}
